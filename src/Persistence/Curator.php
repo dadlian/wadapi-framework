a25efb878815c995d18e9382e38d12e7d39834de
dadlian <sveninem@gmail.com>
2022-03-21 14:44:54 +0000

    Changed default charset for new tables.

--- src/Persistence/Curator.php
+++ src/Persistence/Curator.php
@@ -150,7 +150,7 @@
 			$createStatement .= ")";
 
 			if(!DatabaseAdministrator::isSQLServer()){
-				$createStatement .= "ENGINE=InnoDB DEFAULT CHARSET=latin1";
+				$createStatement .= "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
 			}
 
 			DatabaseAdministrator::execute($createStatement);
@@ -228,7 +228,7 @@
 				$createStatement .= ")";
 
 				if(!DatabaseAdministrator::isSQLServer()){
-					$createStatement .= "ENGINE=InnoDB DEFAULT CHARSET=latin1";
+					$createStatement .= "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
 				}
 
 				DatabaseAdministrator::execute($createStatement);
@@ -289,7 +289,7 @@
 					($property->getAnnotation()->isInteger() && (!preg_match("/^int/", $oldColumnType))) ||
 					(($property->getAnnotation()->isFloat() || $property->getAnnotation()->isMonetary())
 						&& $oldColumnType != "float") ||
-					($property->getAnnotation()->isBoolean() && (!preg_match("/^bit/", $oldColumnType))) ||
+					($property->getAnnotation()->isBoolean() && (!preg_match("/^tinyint\(1\)/", $oldColumnType))) ||
 					($property->getAnnotation()->isText() && !preg_match("/^(text|varchar)/", $oldColumnType)) ||
 					($property->getAnnotation()->isString() && !$property->getAnnotation()->isText() && !preg_match("/^varchar/", $oldColumnType))){
 					//Set all values in changed column to NULL to ensure compatability with new type
