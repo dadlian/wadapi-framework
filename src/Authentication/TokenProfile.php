4cff47dec4c293fb99356e0232aabe545cf1819a
dadlian <sveninem@gmail.com>
2024-09-30 12:15:15 +0100

    Changed initialise function visibility to match parent

--- src/Authentication/TokenProfile.php
+++ src/Authentication/TokenProfile.php
@@ -4,7 +4,7 @@
   use Wadapi\Http\Resource;
 
   abstract class TokenProfile extends Resource{
-    protected function initialise($arguments,$token){
+    public function initialise($arguments,$token){
       $invalidArguments = $this->getInvalidArguments($arguments,$token);
       if(!$invalidArguments){
         $this->build($arguments,$token);
